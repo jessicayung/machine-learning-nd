ea79f0733319878c197042f4de586e33e3295b0c
Jessica Yung <jy357@cam.ac.uk>
2016-09-16 09:51:28 +0100

    project 4 Q1, part of Q2

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -4,7 +4,7 @@ from planner import RoutePlanner
 from simulator import Simulator
 
 class LearningAgent(Agent):
-    """An agent that learns to drive in the smartcab world."""
+    """ An agent that learns to drive in the smartcab world."""
 
     def __init__(self, env):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
@@ -23,9 +23,10 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
+        # self.state = [self.inputs[]]
         
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice([None, 'forward', 'left', 'right'])
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -41,11 +42,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.1, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
